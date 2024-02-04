83ac9336b19625c2074f07a520bdb70593b7f8b2
Mohit Kumar <69501650+hellomohitsangwan@users.noreply.github.com>
2024-02-04 11:05:45 +0530

    new domain add

--- cors/index.js
+++ cors/index.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: ['http://localhost:3000','https://procommerce.netlify.app','https://procommerce.netlify.app/'],
+    origin: ['http://localhost:3000', 'https://shopkart08.netlify.app/', 'https://procommerce.netlify.app','https://procommerce.netlify.app/'],
     optionsSuccessStatus: 200
 };
 
