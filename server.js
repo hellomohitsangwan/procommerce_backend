da41f660be2039a7245e4371fcab2184933d7f92
Mohit Kumar <69501650+hellomohitsangwan@users.noreply.github.com>
2024-02-04 14:44:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,12 @@ const corsMiddleware = require("./cors/index.js");
 const app = express();
 const PORT = process.env.PORT || 8000; // Use environment variable or default to 8000
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://shopkart08.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(fileUpload());
